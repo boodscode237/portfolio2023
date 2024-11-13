59f40ce74679e87e8b31f936b06f9c7da96cf009
BriceDon <65740750+boodscode237@users.noreply.github.com>
2024-11-13 15:38:25 +0300

    Update Home.js

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -15,16 +15,16 @@ const Home = () => {
     return (
         <React.Fragment>
             <CssBaseline />
-            <div className={classes.root}>
+            <div className={`${classes.root} gradient-background`}>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
-                        <Paper className={classes.paper}>
-                            <h1>
-                                Hi, I’m <span className={classes.span}>Brice Дональд</span>
+                        <Paper className={`${classes.paper} intro-paper`}>
+                            <h1 className="intro-title">
+                                Hi, I’m <span className="name-highlight">Brice Дональд</span>
                             </h1>
-                            <h2>
+                            <h2 className="specialization">
                                 I specialize in
-                                <span className={classes.span}>
+                                <span className="typing-highlight">
                                     <Typewriter 
                                         words={[
                                             " Python Programming", 
@@ -44,23 +44,24 @@ const Home = () => {
                         </Paper>
                     </Grid>
                     <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <div className='languages'>
-                                <p>I use various programming languages and tools:</p>
-                                <span className={classes.span}>const languages = &#123;<br />
+                        <Paper className={`${classes.paper} languages-paper`}>
+                            <div className="languages">
+                                <p className="languages-header">I use various programming languages and tools:</p>
+                                <span className="languages-code">
+                                    const stack = &#123;<br />
                                     python: "intermediate",<br />
-                                    javascript: "intermediate",<br />
-                                    Csharp: "beginner",<br />
-                                    kotlin: "beginner",<br />
-                                &#125;
+                                    Pytorch: "intermediate",<br />
+                                    spark: "beginner",<br />
+                                    hadoop: "beginner",<br />
+                                    &#125;
                                 </span>
-                                <p className={classes.span}>
+                                <p className="languages-append">
                                     <Typewriter 
                                         words={[
-                                            ".append('Spark')", 
-                                            ".append('Python')", 
                                             ".append('Pytorch')", 
-                                            ".append('Scikit-Learn')",
+                                            ".append('Python')", 
+                                            ".append('Docker')", 
+                                            ".append('FastAPI')",
                                             ".append('Keras')", 
                                             ".append('NLP')"
                                         ]} 
@@ -77,40 +78,34 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={6}>
                         <Paper className={classes.paper}>
-                            <div className="languages">
-                                <ButtonBase className={classes.image}>
-                                    <img className={classes.img} alt="Brice Donald" src="https://i.ibb.co/LQjg4QJ/linkedin-profile.jpg" />
+                            <div className="profile-images">
+                                <ButtonBase className="profile-image-container">
+                                    <img className="profile-image" alt="Brice Donald" src="https://i.ibb.co/LQjg4QJ/linkedin-profile.jpg" />
                                 </ButtonBase>
-                                <ButtonBase className={classes.image}>
-                                    <img className={classes.img} alt="Brand logo" src="https://i.postimg.cc/TPkSMDz8/instagram-profile-image.png" />
+                                <ButtonBase className="profile-image-container">
+                                    <img className="profile-image" alt="Brand logo" src="https://i.postimg.cc/TPkSMDz8/instagram-profile-image.png" />
                                 </ButtonBase>
                             </div>
                         </Paper>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <SocialIcon url={'https://github.com/boodscode237'} />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <SocialIcon url={'https://twitter.com/LiubliuYa'} />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <SocialIcon url={'https://www.linkedin.com/in/brice-abodo-7995061b0'} />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Paper className={classes.paper}>
-                            <SocialIcon url={'https://www.facebook.com/don.bri.965'} />
-                        </Paper>
+                    <Grid container justifyContent="center" spacing={2} className="social-icons">
+                        <Grid item>
+                            <SocialIcon url="https://github.com/boodscode237" bgColor="#333" />
+                        </Grid>
+                        <Grid item>
+                            <SocialIcon url="https://twitter.com/LiubliuYa" bgColor="#1DA1F2" />
+                        </Grid>
+                        <Grid item>
+                            <SocialIcon url="https://www.linkedin.com/in/brice-abodo-7995061b0" bgColor="#0A66C2" />
+                        </Grid>
+                        <Grid item>
+                            <SocialIcon url="https://www.facebook.com/don.bri.965" bgColor="#4267B2" />
+                        </Grid>
                     </Grid>
                 </Grid>
             </div>
         </React.Fragment>
     );
-}
+};
 
 export default Home;

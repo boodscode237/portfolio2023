a5a3e2c6879984eea874bff262b7e6e2479739fc
BriceDon <65740750+boodscode237@users.noreply.github.com>
2024-11-13 15:23:37 +0300

    Update Home.js

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -4,48 +4,73 @@ import {
     Grid,
     Paper,
 } from "@material-ui/core";
-import { Typewriter } from "react-simple-typewriter"
+import { Typewriter } from "react-simple-typewriter";
 import { SocialIcon } from 'react-social-icons';
 import homeStyles from "./Home.Styles";
 import React from "react";
-import './Home.css'
+import './Home.css';
 
 const Home = () => {
-    const classes = homeStyles()
-    return(
+    const classes = homeStyles();
+    return (
         <React.Fragment>
             <CssBaseline />
-        <div className={classes.root}>
+            <div className={classes.root}>
                 <Grid container spacing={3}>
                     <Grid item xs={12}>
                         <Paper className={classes.paper}>
                             <h1>
                                 Hi, I’m <span className={classes.span}>Brice Дональд</span>
-
                             </h1>
                             <h2>
-                                I know
+                                I specialize in
                                 <span className={classes.span}>
-                <Typewriter words={[" Python", " Data analysis libraries.", " Machine learning libraries(scikit-learn)."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
-              </span>
+                                    <Typewriter 
+                                        words={[
+                                            " Python Programming", 
+                                            " Data Analysis Libraries", 
+                                            " Machine Learning (scikit-learn, TensorFlow)",
+                                            " Artificial Intelligence"
+                                        ]} 
+                                        loop 
+                                        cursor 
+                                        cursorStyle='|' 
+                                        typeSpeed={70} 
+                                        deleteSpeed={50} 
+                                        delaySpeed={1000} 
+                                    />
+                                </span>
                             </h2>
                         </Paper>
                     </Grid>
                     <Grid item xs={6}>
-                        <Paper className={`${classes.paper}`}>
-                            <div className={'languages'}>
-
-                                <p>I use various languages to code: </p>
-                                <span className={classes.span}>let languages = &#123;<br/>
-                                python: "intermediate",<br/>
-                                javascript: "intermediate",<br/>
-                                Csharp: "beginner",<br/>
-                                Kotlin: "beginner",<br/>
-                                    &#125;
-            </span>
+                        <Paper className={classes.paper}>
+                            <div className='languages'>
+                                <p>I use various programming languages and tools:</p>
+                                <span className={classes.span}>const languages = &#123;<br />
+                                    python: "intermediate",<br />
+                                    javascript: "intermediate",<br />
+                                    Csharp: "beginner",<br />
+                                    kotlin: "beginner",<br />
+                                &#125;
+                                </span>
                                 <p className={classes.span}>
-                                    <Typewriter words={[".append('Javascript')", ".append('Python')", ".append('C#')", ".append('ReactJS')",
-                                        ".append('NodeJS')", ".append('Kotlin')" ]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
+                                    <Typewriter 
+                                        words={[
+                                            ".append('Javascript')", 
+                                            ".append('Python')", 
+                                            ".append('C#')", 
+                                            ".append('ReactJS')",
+                                            ".append('NodeJS')", 
+                                            ".append('Kotlin')"
+                                        ]} 
+                                        loop 
+                                        cursor 
+                                        cursorStyle='|' 
+                                        typeSpeed={70} 
+                                        deleteSpeed={50} 
+                                        delaySpeed={1000} 
+                                    />
                                 </p>
                             </div>
                         </Paper>
@@ -54,39 +79,38 @@ const Home = () => {
                         <Paper className={classes.paper}>
                             <div className="languages">
                                 <ButtonBase className={classes.image}>
-                                    <img className={classes.img} alt="my=e donald" src="https://i.ibb.co/LQjg4QJ/linkedin-profile.jpg" />
+                                    <img className={classes.img} alt="Brice Donald" src="https://i.ibb.co/LQjg4QJ/linkedin-profile.jpg" />
                                 </ButtonBase>
                                 <ButtonBase className={classes.image}>
-                                    <img className={classes.img} alt="logo of my brand" src="https://i.postimg.cc/TPkSMDz8/instagram-profile-image.png" />
+                                    <img className={classes.img} alt="Brand logo" src="https://i.postimg.cc/TPkSMDz8/instagram-profile-image.png" />
                                 </ButtonBase>
                             </div>
-
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper className={classes.paper}>
-                            <SocialIcon url={'https://github.com/boodscode237'}/>
+                            <SocialIcon url={'https://github.com/boodscode237'} />
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper className={classes.paper}>
-                            <SocialIcon url={'https://twitter.com/LiubliuYa'}/>
+                            <SocialIcon url={'https://twitter.com/LiubliuYa'} />
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper className={classes.paper}>
-                            <SocialIcon url={'https://www.linkedin.com/in/brice-abodo-7995061b0'}/>
+                            <SocialIcon url={'https://www.linkedin.com/in/brice-abodo-7995061b0'} />
                         </Paper>
                     </Grid>
                     <Grid item xs={3}>
                         <Paper className={classes.paper}>
-                            <SocialIcon url={'https://www.facebook.com/don.bri.965'}/>
+                            <SocialIcon url={'https://www.facebook.com/don.bri.965'} />
                         </Paper>
                     </Grid>
                 </Grid>
             </div>
         </React.Fragment>
-    )
+    );
 }
 
-export default Home
+export default Home;

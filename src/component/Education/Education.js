4cf8370a473ce78b1a99ed9dc97ad0bd67de9b94
BriceDon <65740750+boodscode237@users.noreply.github.com>
2024-11-13 15:13:11 +0300

    Update Education.js

--- src/component/Education/Education.js
+++ src/component/Education/Education.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import {ButtonBase, CssBaseline, Grid, Paper, Typography} from "@material-ui/core";
+import React from 'react';
+import { ButtonBase, CssBaseline, Grid, Paper } from "@material-ui/core";
 import educationStyles from "./Education.Styles";
 
-
 const Education = () => {
-    const classes = educationStyles()
-    return(
+    const classes = educationStyles();
+    return (
         <React.Fragment>
             <CssBaseline />
             <div className={classes.root}>
@@ -16,34 +15,31 @@ const Education = () => {
                             <Grid item xs container direction="column" spacing={2}>
                                 <Grid item xs>
                                     <Paper className={classes.paper}>
-                                        <h1>Bachelor Degree in Information Technology - 2023</h1>
-                                        <h2>Russian State Vocational and Pedagogical University</h2>
+                                        <h1>Master's Degree in Artificial Intelligence - Expected 2025</h1>
+                                        <h2>ITMO University, Saint Petersburg</h2>
                                         <p>
-                                            I am actually at my last year of studies at RSVPU in Yekaterinburg.
-                                            I will be writting my diploma on the topic: <b>"Creation and management of a website for a contest"</b>
+                                            Currently pursuing a Master's in Artificial Intelligence, specializing in NLP and Computer Vision. 
+                                            Research topic: <b>"Development and Optimization of a Large Language Model (LLM) for the French Language"</b>.
                                         </p>
                                     </Paper>
                                 </Grid>
                                 <Grid item xs>
                                     <Paper className={classes.paper}>
-                                        <h1>Bachelor Degree in Information Technology - 2016</h1>
-                                        <h2>Catholic university of Bertoua</h2>
+                                        <h1>Bachelor's Degree in Finance and Accounting - 2016</h1>
+                                        <h2>Catholic University of Central Africa, Yaoundé</h2>
                                         <p>
-                                            I passed my bachelor degree with <b>honors and appreciations</b> from the committee.
-                                            During that time I was the delegate(Старость) of my promotion for 2 and the half years.
-                                            I was elected by my classmates two times in a row.
-                                            I filled the role and gain respect from my classmates.
+                                            Graduated with honors. During my studies, I held the role of class delegate, demonstrating leadership 
+                                            and organizational skills.
                                         </p>
                                     </Paper>
                                 </Grid>
-
-                                <Grid item>
+                                <Grid item xs>
                                     <Paper className={classes.paper}>
-                                        <h1>Baccalaureate - Government Certificate Of Education - 2013</h1>
+                                        <h1>Technical Baccalaureate - 2013</h1>
                                         <h2>Minor Seminary Saint Joseph of Efok</h2>
                                         <p>
-                                            In 2013 I passed the Government Certificate Of Education. The exam to pass to study at the University.
-                                            I passed my Exam with honors and I had one of the highest grade in one subject - English.
+                                            Completed with honors, achieving top grades, especially in English. This qualification enabled me to 
+                                            pursue university studies.
                                         </p>
                                     </Paper>
                                 </Grid>
@@ -52,13 +48,13 @@ const Education = () => {
                     </Grid>
                     <Grid xs={6} item>
                         <ButtonBase className={classes.image}>
-                            <img className={classes.img} alt="complex" src="https://i.postimg.cc/nhwm7V7K/logo-transparent.png" />
+                            <img className={classes.img} alt="ITMO logo" src="https://i.postimg.cc/nhwm7V7K/logo-transparent.png" />
                         </ButtonBase>
                     </Grid>
                 </Grid>
             </div>
         </React.Fragment>
-    )
+    );
 }
 
-export default Education
+export default Education;

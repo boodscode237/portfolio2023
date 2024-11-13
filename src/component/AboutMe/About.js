fb274b9f5d3b6c8b0df061fc2c25a66d6114b162
BriceDon <65740750+boodscode237@users.noreply.github.com>
2024-11-13 15:16:34 +0300

    Update About.js

--- src/component/AboutMe/About.js
+++ src/component/AboutMe/About.js
@@ -1,74 +1,54 @@
-import React from 'react'
-import {ButtonBase, CssBaseline, Grid, Paper, Typography} from "@material-ui/core";
+import React from 'react';
+import { ButtonBase, CssBaseline, Grid, Paper } from "@material-ui/core";
 import aboutStyles from "./About.Styles";
 
 const About = () => {
-    const classes = aboutStyles()
-    return(
+    const classes = aboutStyles();
+    return (
         <React.Fragment>
             <CssBaseline />
             <div className={classes.root}>
                 <h1><center>ABOUT ME</center></h1>
-                    <Grid container>
-                        <Grid container xs={7} spacing={2}>
-                            <Grid item xs={6} sm container>
-                                <Grid item xs container direction="column" spacing={2}>
-                                    <Grid item xs>
-                                        <Paper className={classes.paper}>
-                                            <h2>Who Am I?</h2>
-                                            <p>I am a student at the <b>Russian State Vocational and Pedagogical University.</b></p>
-                                            <p>In the past I studied accounting and
-                                                finance and worked a bit in that domain.
-                                            </p>
-                                            <p>Then i decided to learn programming because
-                                                with programming people make <br /><b>MAGIC</b> and <b>Coffee</b>
-                                            </p>
-                                        </Paper>
-                                    </Grid>
-                                    <Grid item xs>
-                                        <Paper className={classes.paper}>
-                                            <h2>What I like</h2>
-                                            <p>For sure, I like languages, I like challenges, and food. My life is:<br/>
-                                            Study, sport, cooking(eating 😋😋), Study. I read when I am in public transport.
-                                                Recently I spent a lot of time reading <b>Dostoevsky</b></p>
-                                            <p>
-                                                I can say the programming laguage I like the most is Python but I code mostly with javascript.
-                                                Recently I started making Android apps with kotlin jetpack compose. It is just 🔥🔥.
-                                            </p>
-                                        </Paper>
-                                    </Grid>
+                <Grid container>
+                    <Grid container xs={7} spacing={2}>
+                        <Grid item xs={6} sm container>
+                            <Grid item xs container direction="column" spacing={2}>
+                                <Grid item xs>
+                                    <Paper className={classes.paper}>
+                                        <h2>Who Am I?</h2>
+                                        <p>I am a graduate student at <b>ITMO University</b> in Saint Petersburg, specializing in <b>Artificial Intelligence</b>.</p>
+                                        <p>Previously, I studied finance and accounting, gaining valuable experience in that field before discovering my passion for programming and AI.</p>
+                                        <p>This journey led me to focus on AI, particularly <b>Natural Language Processing (NLP)</b> and <b>Computer Vision</b>, where I believe programming can truly create <b>MAGIC</b> (and, of course, coffee!).</p>
+                                    </Paper>
+                                </Grid>
+                                <Grid item xs>
+                                    <Paper className={classes.paper}>
+                                        <h2>What I Like</h2>
+                                        <p>I enjoy learning new languages, taking on challenges, and exploring different cuisines. My routine is all about study, sports, and cooking (and eating 😋).</p>
+                                        <p>I spend a lot of time reading, especially Dostoevsky recently, while commuting. My favorite programming language is Python, although I often work with JavaScript. Lately, I’ve been delving into Android development with Kotlin and Jetpack Compose, which is just 🔥🔥.</p>
+                                    </Paper>
+                                </Grid>
 
-                                    <Grid item>
-                                        <Paper className={classes.paper}>
-                                            <h2>My plans</h2>
-                                            <p>
-                                                I have plans for the future.
-                                                I want to have a Master degree and even a PHD if all goes well.<br/>
-                                                I want to teach students at the university, online and even in school.
-                                                I plan to create a site for open online courses in my country.
-                                                A site that can be used by students from primary school till the
-                                                university to learn and practice what they learned at school.
-                                                At least a place were any question can be answered.
-                                            </p>
-                                            <p>Also with some friends we are looking to create a startup with the goal
-                                                of bringing solution to problems that our countries face(Creating technologies for that).
-                                                <br/>
-                                                We just want to contribute as much as we can.
-                                            </p>
-                                        </Paper>
-                                    </Grid>
+                                <Grid item>
+                                    <Paper className={classes.paper}>
+                                        <h2>My Plans</h2>
+                                        <p>Looking ahead, I aim to complete a Master’s and eventually pursue a PhD. I aspire to teach AI and programming at the university level and make education accessible through online courses.</p>
+                                        <p>One of my dreams is to create a platform for open online courses in my home country, supporting students from primary school through university, where they can ask questions and gain practical skills.</p>
+                                        <p>Additionally, I’m working with friends to start a technology-focused company aimed at solving challenges in our communities. We hope to make a meaningful impact by developing innovative solutions for local problems. Together, we’re excited to contribute as much as possible.</p>
+                                    </Paper>
                                 </Grid>
                             </Grid>
                         </Grid>
-                        <Grid xs={5} item>
-                            <ButtonBase className={classes.image}>
-                                <img className={classes.img} alt="complex" src="https://i.postimg.cc/yYjc1y8b/pinterest-board-photo.png" />
-                            </ButtonBase>
-                        </Grid>
                     </Grid>
-                </div>
+                    <Grid xs={5} item>
+                        <ButtonBase className={classes.image}>
+                            <img className={classes.img} alt="About Me" src="https://i.postimg.cc/yYjc1y8b/pinterest-board-photo.png" />
+                        </ButtonBase>
+                    </Grid>
+                </Grid>
+            </div>
         </React.Fragment>
-    )
+    );
 }
 
-export default About
+export default About;
